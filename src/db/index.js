da0f7df6b1195026cbf6b8d8594a765065a1fb9c
devsaini08 <devsaini143278@gmail.com>
2024-06-13 17:04:51 +0530

    project connected with database

---
+++ src/db/index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+// const PORT = process.env.PORT;
+// const URL = process.env.MONGODB_URI;
+
+const PORT = 8000;
+const URL = "mongodb+srv://dev:devsaini08@cluster0.dch1s6p.mongodb.net";
+
+
+
+const connectDB = async () => {
+    try {
+        const coonectioninstance = await mongoose.connect(`${URL}/${DB_NAME}`)
+        console.log(`connect sucess  ${coonectioninstance.connection.host}`);
+    } catch (error) {
+        console.log("mongo connecting err", error);
+        process.exit(1)
+    }
+}
+
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,1 +1,82 @@
-console.log("hello index.js");
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+
+// import express from "express";
+
+// const app = express();
+// console.log(app);
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`app is listening on port ${process.env.PORT}`);
+//         })
+//         app.on("error", (error) => {
+//             console.log("ERROR", error);
+//             throw error
+//         })
+
+
+
+//     } catch (error) {
+//         console.error("ERROR", error)
+//         // throw err
+//     }
+// })()
+
+
+// const  connectDB = () => {
+//     try {
+//          mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`app is listening on port ${process.env.PORT}`);
+//         })
+//         app.on("error", (error) => {
+//             console.log("ERROR", error);
+//             throw error
+//         })
+
+
+
+//     } catch (error) {
+//         console.error("ERROR", error)
+//         // throw err
+//     }
+// }
+
+// connectDB()
+
+
+
+// const url = "ongodb+srv://dev:devsaini08@cluster0.dch1s6p.mongodb.net"
+// const PORT = 8000;
+
+
+// console.log(url);
+// console.log(PORT);
+
+// mongoose.connect(url)
+//     .then(() => {
+//         app.listen(PORT, () => {
+//             console.log(`server is runing and up`)
+//             // console.log("connect to database");
+//         })
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
